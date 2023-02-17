8c082f44a8af34f9f9d6b76696cd6c24a11fe5a2
vbabulreddy <110149047+vbabulreddy@users.noreply.github.com>
2023-02-17 18:10:44 +0530

    Update Vnet.tf

--- Vnet.tf
+++ Vnet.tf
@@ -1,11 +1,14 @@
-resource "azurerm_resource_group" "Rg1" {
-    name = "East-us-rg1"
-    location = "East Us"
-
-  
+# Vnet creation 
+resource "azurerm_virtual_network" "Vnet-03" {
+  name                = var.Vnet_name
+  location            = var.WU-RG_location
+  resource_group_name = var.WU-RG_name
+  address_space       = var.vnet_range
 }
 
-#resource "azurerm_resource_group" "Rg2" {
-#  name     = "example"
-#  location = "West Europe"
-#}
+resource "azurerm_subnet" "Subnet_01" {
+  name                 = var.Subnet_1
+  resource_group_name  = var.WU-RG_name
+  virtual_network_name = var.Vnet_name
+  address_prefixes     = var.Internal
+}
